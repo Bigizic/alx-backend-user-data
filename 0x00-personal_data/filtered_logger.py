7f9e61cbefac5e765f34e225f0bc5f89ef687530
Bigizic <olalekanisaac75@gmail.com>
2024-01-10 21:42:50 +0000

    updates

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,6 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     """ uses regex to replace occurences of certain fled values """
-    return re.sub(r'(\b(?:{}))=(.*?)(?={}|$)'.format(
-                  '|'.join(fields), re.escape(separator)),
-                  lambda match: match.group(1) + '=' + redaction,
-                  message)
+    return re.sub(r'(\b(?:{}))=(.*?)(?={}|$)'.format('|'.join(fields),
+                  re.escape(separator)), lambda match: match.group(1)
+                  + '=' + redaction, message)
