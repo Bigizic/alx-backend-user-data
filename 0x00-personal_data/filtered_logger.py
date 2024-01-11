a9ed7f724bf1c37e45bdf8c43547824359649ee3
Bigizic <olalekanisaac75@gmail.com>
2024-01-11 11:49:34 +0000

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-""" a function called filter_datum that returns the log message obfuscated
+"""A function called filter_datum that returns the log message obfuscated
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ uses regex to replace occurences of certain fled values """
     return re.sub(r'(\b(?:{}))=(.*?)(?={}|$)'.format('|'.join(fields),
                   re.escape(separator)), lambda match: match.group(1)
