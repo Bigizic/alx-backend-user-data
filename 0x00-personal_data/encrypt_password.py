e8d9d6f6de176b8f204f3e00760a52b1d4591fac
Bigizic <olalekanisaac75@gmail.com>
2024-01-11 17:06:16 +0000

    new files

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     """returns a salted bcrypt hash
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a password matches a hashed password
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
