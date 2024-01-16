799a30f71a752db48bf2ca169ccf05c098ed7ff2
Bigizic <olalekanisaac75@gmail.com>
2024-01-16 01:19:06 +0000

    Update api

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,8 +27,6 @@ if auth_type:
 def handler() -> None:
     """ Filters each request
     """
-    global auth
-
     path = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if auth is None:
@@ -38,10 +36,10 @@ def handler() -> None:
         return
 
     if auth.authorization_header(request) is None:
-            abort(401)
+        abort(401)
 
     if auth.current_user(request) is None:
-            abort(403)
+        abort(403)
 
 
 @app.errorhandler(401)
