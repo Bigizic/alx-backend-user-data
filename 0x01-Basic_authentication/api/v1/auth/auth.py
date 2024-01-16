2445062cd82befcc67c4cc00769e5430aa99c712
Bigizic <olalekanisaac75@gmail.com>
2024-01-16 00:34:25 +0000

    Update api

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,21 @@ class Auth():
         Return:
             - False - path and excluded_paths
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path not in excluded_paths:
+            return True
+
+        new_path = path[-1]
+        if new_path != '/':
+            new_path = path + '/'
+        else:
+            new_path = path
+
+        if new_path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
