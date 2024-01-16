03e4c0a97e0f60f6f1b45bec3c88cdafb9f60d94
Bigizic <olalekanisaac75@gmail.com>
2024-01-16 01:06:15 +0000

    Update api

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,25 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = os.getenv("AUTH_TYPE", None)
+if auth:
+    from api.vi.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def handler() -> None:
+    """ Filters each request
+    """
+    path = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        return
+    if not auth.require_auth(request.path, path):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(401)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -44,7 +44,9 @@ class Auth():
         Return:
             - None - request (will be the Flask request object)
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
