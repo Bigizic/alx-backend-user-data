8e8dfb6bd2623d478c18b3af1b1e2f06af0020d3
Bigizic <olalekanisaac75@gmail.com>
2024-01-18 18:32:58 +0000

    Update api

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""A an expiration date to a Session ID subclass of SessionAuth
+"""
+
+from .session_auth import SessionAuth
+from datetime import datetime, timedelta
+from os import environ
+
+
+class SessionExpAuth(SessionAuth):
+    """Implementation
+    """
+
+    def __init__(self):
+        """Constructor
+        """
+        super().__init__()
+        duration = environ.get('SESSION_DURATION')
+        if duration is None or not int(duration):
+            self.session_duration = 0
+        else:
+            self.session_duration = int(duration)
+
+    def create_session(self, user_id=None):
+        """Implementation
+        """
+        ps = super().create_session(user_id)
+        if not isinstance(ps, str):
+            return None
+        self.user_id_by_session_id[ps] = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+        return ps
+
+    def user_id_for_session_id(self, session_id=None):
+        """Implementation
+        """
+        if session_id is None:
+            return None
+        ps = self.user_id_by_session_id.get(session_id)
+        if ps is None or ps.get('created_at') is None:
+            return None
+        if self.session_duration <= 0:
+            return ps
+        span = timedelta(seconds=self.session_duration)
+        if (ps.get('created_at') + span) < datetime.now():
+            return None
+        return ps.get('user_id')
