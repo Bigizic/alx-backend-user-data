c73aa789f62f171b268d4172e8730ccc2eda9ebe
Bigizic <olalekanisaac75@gmail.com>
2024-01-18 11:59:01 +0000

    Update api

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,23 @@
 """
 
 from api.v1.auth.auth import Auth
+from flask import session
+import uuid
 
 
 class SessionAuth(Auth):
     """Implementation of subclass of Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id
+        Return:
+            - None if user_id is None
+            - None if user_id is not str
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        s_id = str(uuid.uuid4())
+        user_id_by_session[s_id] = user_id
+        return s_id
