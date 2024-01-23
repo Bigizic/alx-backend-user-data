874f722da8b51fd21fe56ddb617ffcacb15a58c8
Bigizic <olalekanisaac75@gmail.com>
2024-01-23 21:53:51 +0000

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,10 +36,12 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Implementation
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
+        if email and hashed_password:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+            return new_user
+        return None
 
     def find_user_by(self, **kwargs) -> User:
         """Locates a user based on arbitrary keywords
