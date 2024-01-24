963b506876d440b7ddf84fa754554d7d9608d9fb
Bigizic <olalekanisaac75@gmail.com>
2024-01-24 02:02:14 +0000

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,7 +88,7 @@ def get_reset_password_token():
 
 
 @app.route('/reset_password', methods=["PUT"], strict_slashes=False)
-def update_password():
+def update_password() -> jsonify:
     email = request.form.get('email')
     rst = request.form.get('reset_token')
     pwd = request.form.get('new_password')
