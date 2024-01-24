2dbde4801d1e85b69e37d8bce216c11c3500f5dd
Bigizic <olalekanisaac75@gmail.com>
2024-01-24 00:11:34 +0000

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,5 +30,5 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
