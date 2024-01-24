f8c65d226efdffb4047d7a7718fdcf391dbb3b67
Bigizic <olalekanisaac75@gmail.com>
2024-01-24 00:05:36 +0000

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -65,7 +65,7 @@ class DB:
         except InvalidRequestError as e:
             raise e
 
-    def update_user(self, user_id, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """Updates a user based on user_id
         """
         try:
