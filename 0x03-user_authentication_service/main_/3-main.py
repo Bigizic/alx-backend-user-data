de80611d3c8d42256bd2b3220efb0415e6384d32
Bigizic <olalekanisaac75@gmail.com>
2024-01-24 00:20:05 +0000

    add files

--- 0x03-user_authentication_service/main_/3-main.py
+++ 0x03-user_authentication_service/main_/3-main.py
@@ -13,12 +13,25 @@ my_db = DB()
 
 email = 'test@test.com'
 hashed_password = "hashedPwd"
+ema = "ola@gmail"
+pwd = "run"
 
 user = my_db.add_user(email, hashed_password)
+ur = my_db.add_user(ema, pwd)
 print(user.id)
+print("-----------")
+print(ur.id)
+print(ur.hashed_password)
 
 try:
     my_db.update_user(user.id, hashed_password='NewPwd')
     print("Password updated")
+except ValueError:
+    print("Error")
+
+try:
+    my_db.update_user(ur.id, age=3,  hashed_password="goat", session_id="something")
+    print(ur.hashed_password, ur.session_id)
+    print("password changed")
 except ValueError:
     print("Error")
