d8ef5cccca8cf4c42c1f57a559b50580802be03e
Bigizic <olalekanisaac75@gmail.com>
2024-01-24 00:09:26 +0000

    Update app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env puython3
+"""Basic Flask APP
+"""
+
+from auth import Auth
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def basic_flask_app():
+    """Implementation
+    """
+    return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=["POST"])
+def register_user():
+    """Registers a user
+    """
+    AUTH = Auth()
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+
+    try:
+        registor = AUTH.register_user(email, pwd)
+        return jsonify({'email': email, 'message': 'user created'})
+    except ValueError as e:
+        return jsonify({"message": "email already registered"}), 400
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
