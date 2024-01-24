7487bd951e31e3a0a1e1db0e0285704a272b5c4c
Bigizic <olalekanisaac75@gmail.com>
2024-01-24 00:08:47 +0000

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -23,7 +23,7 @@ class Auth:
         """
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
+    def register_user(self, email: str, password: str) -> User:
         """ Implementation
         """
         try:
